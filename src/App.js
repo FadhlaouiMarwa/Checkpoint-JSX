e232f084f7deb7506774cec7f6c8d659c18ff332
Fadhlaoui Marwa <fadhlaoui.marwa@gmail.com>
2022-01-09 04:00:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Imagesrc from './Imagesrc.jpeg'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+
+<h1 class="title red" style={{color:"#FF33E6"}} >Checkpoint Fadhlaoui Marwa JSX</h1>
+
+<br/>
+
+<img src="/ImagePublic.jpg" / >
+
+<br/>
+
+<img src={Imagesrc}/ >
+
+</div>
+
+<vidéo width="320" height="240" contrôles>
+
+<source src="maVidéo.mp4" type="video/mp4"/ >
+
+</vidéo>
     </div>
   );
 }
